64df478b1e5ba876647effe67db0c6524adb1d72
Derek <derek.yadlowski@sait.ca>
2025-11-28 09:03:45 -0700

    Context Examples - Dark Mode

--- App.jsx
+++ App.jsx
@@ -1,4 +1,3 @@
-
 import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
@@ -7,23 +6,26 @@ import ContactScreen from './src/screens/ContactScreen';
 import CounterScreen from './src/screens/CounterScreen';
 import AdoptionScreen from './src/screens/AdoptionScreen';
 import GalleryScreen from './src/screens/GalleryScreen';
+import ThemeLayout from './src/layouts/theme-layout';
 
-const RootStack = createNativeStackNavigator( {
+const RootStack = createNativeStackNavigator({
+  layout: ThemeLayout,
   screens: {
     Home: HomeScreen,
     About: AboutScreen,
     Contact: ContactScreen,
     Counter: CounterScreen,
     Adopt: AdoptionScreen,
-    Gallery: GalleryScreen
-  }
-} );
+    Gallery: GalleryScreen,
+  },
+});
 
 const Navigation = createStaticNavigation(RootStack);
 
 function App() {
-  return <Navigation />
+  return (
+      <Navigation />
+  );
 }
-
 
 export default App;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,13 +1,17 @@
-import { Button, ScrollView, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
 import MyFirstComponent from '../components/my-component';
 import { useNavigation } from '@react-navigation/native';
+import { useMyTheme } from '../context/theme-context';
 
 export default function HomeScreen() {
   const nav = useNavigation();
+  const {themeStyles, toggleDarkMode} = useMyTheme();
 
   return (
-    <View style={{ padding: 20}}>
+    <View style={ [homeStyles.container, themeStyles.container] }>
       <ScrollView>
+        <Text style={ [{fontSize:30, textAlign:'center'}, themeStyles.text] } >Hello World!</Text>
+        <Button title="Dark Mode" onPress={toggleDarkMode} />
         <Button title="API Art Gallery" onPress={() => nav.navigate('Gallery')} />
         <Button title="Dog Adoption" onPress={() => nav.navigate('Adopt')} />
         <Button title="Go to About" onPress={() => nav.navigate('About')} />
@@ -18,3 +22,9 @@ export default function HomeScreen() {
     </View>
   );
 }
+
+const homeStyles = StyleSheet.create({
+  container: {
+    padding:20
+  }
+})
